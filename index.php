77200795c2e15e0bf7d346fd3e6a2de0931bb77b
SimpleCycy <77782606+SimpleCycy@users.noreply.github.com>
2021-01-21 23:44:51 +0800

    table task initial

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
         <title> ToDo List Application PHP and MySQL </title>
         <link rel = "stylesheet" type="text/css" href="style.css">
     </head>
-<body>
+<body align ="center">
+
     <div class = "heading">
         <h2> ToDo List Application PHP and MySQL </h2>
     </div>
@@ -13,5 +14,25 @@
             <input type ="text" name="task" class ="task_input" placeholder ="Please Input the Task">
             <button type="submit" class ="add_btn"name="_submit"> Add Task</button>
         </form>
+<br><br>
+        <table align = "center">
+            <thead>
+                <tr>
+                    <th>N</th>
+                    <th>Task</th>
+                    <th>Action</th>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1<td>
+                    <td class="task_input">This is the first task place holder</td>
+                    <td class= "delete">
+                        <a href="#">X</a>
+                    </td>
+                </tr>
+            </tbody>
+
+        </table>
 </body>
 </html>
