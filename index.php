0a04431b7de100e44aabcfc4c8f49310ebf645e1
SimpleCycy <77782606+SimpleCycy@users.noreply.github.com>
2021-01-21 22:38:56 +0800

    Headings

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> ToDo List Application PHP and MySQL </title>
+        <link rel = "stylesheet" type="text/css" href="style.css">
+    </head>
+<body>
+    <div class = "heading">
+        <h2> ToDo List Application PHP and MySQL </h2>
+    </div>
+        <form method ="POST" action="index.php">
+            <input type ="text" name="task" class="task_input">
+            <button type="submit" class ="add_btn"name="_submit"> Add Task</button>
+        </form>
+</body>
+</html>
