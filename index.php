e69a4a6b30715db319c97cc16b9f3e7031e80f97
SimpleCycy <77782606+SimpleCycy@users.noreply.github.com>
2021-01-22 13:24:12 +0800

    Error Handling if input is empty

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
+    $errors = "";
     //connect to the database
     $db = mysqli_connect('localhost','root','', 'todo');
 
     if (isset($_POST['submit'])){
         $task = $_POST['task'];
+        if (empty($task)){
+            $errors = "You must fill in the task";
+        }else{
 
         mysqli_query($db,"INSERT INTO tasks (task) VALUES('$task')");
         header('location: index.php');
+        }
     }
+    $tasks = mysqli_query($db, "SELECT * FROM tasks");
    
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,33 +24,40 @@
         <link rel = "stylesheet" type="text/css" href="style.css">
     </head>
 <body>
-
+    <center>
     <div class = "heading">
         <h2> ToDo List Application PHP and MySQL </h2>
     </div>
     <br>
         <form method ="POST" action="index.php">
+        <?php if(isset($errors)){ ?>
+            <p><?php echo $errors; ?></p>
+        <?php } ?>
             <input type ="text" name="task" class ="task_input" placeholder ="Please Input the Task">
             <button type="submit" class ="add_btn"name="submit"> Add Task</button>
         </form>
-        <table align = "center">
+        <table>
             <thead>
                 <tr>
                     <th>N</th>
                     <th>Task</th>
                     <th>Action</th>
+                </tr>
             </thead>
 
             <tbody>
-                <tr>
-                    <td>1<td>
-                    <td class="task">This is the first task place holder</td>
-                    <td class= "delete">
-                        <a href="#">X</a>
-                    </td>
-                </tr>
-            </tbody>
+                <?php while ($row = mysqli_fetch_array($tasks)){ ?>
+                    <tr>
+                        <td><?php echo $row['id']; ?><td>
+                        <td class="task"><?php echo $row['task']; ?></td>
+                        <td class= "delete">
+                            <a href="#">X</a>
+                        </td>
+                    </tr>
 
+                <?php }  ?>
+                </tbody>
+                </center>
         </table>
 </body>
 </html>
