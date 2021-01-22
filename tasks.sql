717d87bec6d8ebb76eb3784c3eeeea7177419d3a
SimpleCycy <77782606+SimpleCycy@users.noreply.github.com>
2021-01-22 17:11:17 +0800

    Task table finale with inputs

--- tasks.sql
+++ tasks.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 21, 2021 at 02:26 PM
+-- Generation Time: Jan 22, 2021 at 10:10 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
@@ -34,6 +34,20 @@ CREATE TABLE `tasks` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `task`) VALUES
+(6, 'Added to the Database'),
+(7, 'Another Database Added'),
+(8, 'Deleting task'),
+(10, 'The numbering fix'),
+(11, 'Everything is working'),
+(12, 'Design the Layout'),
+(14, 'Alignment of the Table'),
+(15, 'All Done!');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -51,7 +65,7 @@ ALTER TABLE `tasks`
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
